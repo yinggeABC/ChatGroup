3b8bac3fa3adb7c83eb72193a1dc179e915fed7c
yinggeABC <906772533@qq.com>
2016-10-24 20:02:18 +0800

    实现@功能,只有被@的人才能收到消息

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = require("express")();
 var path = require("path");
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
+var sockets ={};
 
 server.listen(8080)
 
@@ -18,9 +19,17 @@ io.on("connection",function(socket){
     var username ='';
     socket.on("message",function(msg){
         if (username){
-            io.emit("message",{user:username,content:msg});
+            var ary = msg.match(/^@(.+):\s/);
+            if (ary) {//@了某人
+                socket.send({user:username,content:msg});
+                sockets[ary[1]].emit("message",{user:username,content:msg});
+            }else{
+                io.emit("message",{user:username,content:msg});
+            }
+
         }else{
             username=msg;
+            sockets[username] = socket;//每新加入一个用户，就将其socket保存起来
             io.emit("message",{user:"系统",content:username+"加入聊天室"})
         }
     })
